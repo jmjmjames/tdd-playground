f4701cf2b759f266a629a478a585a82779b52208
jmjmjames <chungjm0711@gmail.com>
2022-11-04 11:29:26 +0900

    test: assertj dependency 체크

---
+++ src/test/java/chap01/StringTest.java
@@ -0,0 +1,13 @@
+package chap01;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringTest {
+
+    @Test
+    void substring() {
+        String str = "abcde";
+        Assertions.assertThat("cd").isEqualTo(str.substring(2, 4));
+    }
+}
