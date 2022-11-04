336aa90cca02869a5f147b70315d195546f17c67
jmjmjames <chungjm0711@gmail.com>
2022-11-04 11:31:09 +0900

    test: 문제 요구 사항

---
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -0,0 +1,20 @@
+package chap02;
+
+import org.junit.jupiter.api.Test;
+
+public class PasswordStrengthMeterTest {
+    /**
+     * <p>아래의 세 가지 규칙을 모두 충족하면 암호는 강함.</p>
+     * <p>2개의 규칙을 충족하면 암호는 보통.</p>
+     * <p>1개 이하의 규칙을 충족하면 암호는 약함.</p>
+     * <ul>
+     *  <li>길이가 8글자 이상</li>
+     *  <li>0~9 사이의 숫자를 포함</li>
+     *  <li>대문자를 포함 </li>
+     * </ul>
+     */
+    @Test
+    void name() {
+
+    }
+}
