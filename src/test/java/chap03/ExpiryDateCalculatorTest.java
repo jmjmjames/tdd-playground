6a68438073707fd66e7be216cb604eecb94b48d5
jmjmjames <chungjm0711@gmail.com>
2022-11-05 03:51:46 +0900

    test: 코드 정리 중복을 제거
    다만 코드를 이해하려면 어떤 것이 billingDate 이며
    expectedExpiryDate인지 확인을 해야한다.

--- src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -11,20 +11,22 @@ public class ExpiryDateCalculatorTest {
     @Test
     @DisplayName("만원 납부하면 한달 뒤가 만료일이 됨")
     void pay_10000_won_Then_expiry_date_should_be_one_month_later() {
-        LocalDate billingDate = LocalDate.of(2022, 11, 4);
-        int payAmount = 10_000;
 
-        ExpiryDateCalculator calculator = new ExpiryDateCalculator();
-        LocalDate expiryDate = calculator.calculateExpiryDate(billingDate, payAmount);
+        assertExpiryDate(
+                LocalDate.of(2022, 11, 4),
+                10_000,
+                LocalDate.of(2022, 12, 4));
 
-        assertThat(expiryDate).isEqualTo(LocalDate.of(2022, 12, 4));
-
-        LocalDate billingDate2 = LocalDate.of(2022, 11, 11);
-        int payAmount2 = 10_000;
-
-        LocalDate expiryDate2 = calculator.calculateExpiryDate(billingDate2, payAmount2);
-        assertThat(expiryDate2).isEqualTo(LocalDate.of(2022, 12, 11));
+        assertExpiryDate(
+                LocalDate.of(2022, 11, 11),
+                10_000,
+                LocalDate.of(2022, 12, 11));
     }
 
+    private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
+        ExpiryDateCalculator calculator = new ExpiryDateCalculator();
+        LocalDate realExpiryDate = calculator.calculateExpiryDate(billingDate, payAmount);
+        assertThat(realExpiryDate).isEqualTo(expectedExpiryDate);
+    }
 
 }
