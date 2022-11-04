8f727b7dca31b1969397efbcdfc13f57193128e0
jmjmjames <chungjm0711@gmail.com>
2022-11-04 13:31:39 +0900

    test: 숫자를 포함하지 않고 나머지 조건은 충족하는 경우

--- src/test/java/chap02/PasswordStrengthMeter.java
+++ src/test/java/chap02/PasswordStrengthMeter.java
@@ -1,7 +1,21 @@
 package chap02;
 
 public class PasswordStrengthMeter {
-    public static PasswordStrength meter(String s) {
-        return null;
+    public static PasswordStrength meter(String password) {
+        if (password.length() < 8) {
+            return PasswordStrength.NORMAL;
+        }
+
+        boolean hasNumber = false;
+        for (char ch : password.toCharArray()) {
+            if (ch >= '0' && ch <= '9') {
+                hasNumber = true;
+                break;
+            }
+        }
+        if (hasNumber) {
+            return PasswordStrength.NORMAL;
+        }
+        return PasswordStrength.STRONG;
     }
 }
